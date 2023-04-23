4d4e8fb7e64b923a7a27c2cc52ad4739b3f7fe82
Muhammad Haghnegahdar <104886566+maxjn@users.noreply.github.com>
2023-04-23 10:21:00 +0330

    useQuery: (#1)
    - getting data using useQuery

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,28 @@ import {
   RouterProvider,
   Route,
 } from "react-router-dom";
+//Layouts
 import HomeLayout from "./layouts/HomeLayout";
+//Pages
 import Planets from "./pages/Planets";
 import People from "./pages/People";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<HomeLayout />}>
-      <Route path="people" element={<People />}>
-        <Route path=":id" element={<People />}></Route>
-      </Route>
-      <Route path="planets" element={<Planets />}>
-        <Route path=":id" element={<People />}></Route>
-      </Route>
+      <Route path="people" element={<People />}></Route>
+      <Route path="planets" element={<Planets />}></Route>
     </Route>
   )
 );
 
 function App() {
   return (
-    <div className="App">
-      <RouterProvider router={router} />
-    </div>
+    <>
+      <div className="App">
+        <RouterProvider router={router} />
+      </div>
+    </>
   );
 }
 
